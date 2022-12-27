786bd00e22d9e5df996e4be1463d5df763934cca
Matei77 <matei.ionescu7@gmail.com>
2022-12-27 15:58:16 +0200

    update server

--- server-client/flask_server.py
+++ server-client/flask_server.py
@@ -1,5 +1,6 @@
 #Package import
-from flask import Flask, render_template, send_file, make_response, url_for, Response, redirect, request 
+from flask import Flask, render_template, request
+from waitress import serve
  
 #initialise app
 app = Flask(__name__)
@@ -9,25 +10,26 @@ app = Flask(__name__)
 def index():
 	return render_template('index.html', PageTitle = "Landing page")
 
-if __name__ == '__main__':
-	app.run(debug = True)
 
-
-#These functions will run when POST method is used.
-@app.route('/', methods = ["POST"] )
+#This function will run when POST method is used.
+@app.route('/', methods = ['POST'] )
 def parse_request():
     #getting question from form
     question = request.form.get('question')
-    
+
     
     #making sure its not empty
     if question != '':
-        processed_text = question.upper()
+        # TODO: generate answer
+        answer = question.upper()
+        
         submitted = True
-        
-        return render_template('index.html', processed_text = processed_text, question = question, submitted = submitted)
-        
-    
+        return render_template('index.html', answer = answer, question = question, submitted = submitted)
+
     else:
         return render_template('index.html', PageTitle = "Landing page")
-      #This just reloads the page if question is given and the user tries to POST. 
+        #This just reloads the page if question is given and the user tries to POST. 
+
+
+if __name__ == '__main__':
+	serve(app, host = "0.0.0.0", port = 8080)
